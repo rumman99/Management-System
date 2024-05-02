bf02cf57d0e451859e3080069261c88bb6327391
rumman99 <tasnimrumman369@gmail.com>
2024-05-03 00:29:37 +0600

    added frontend url on cors

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -15,12 +15,12 @@ const Form = () => {
         e.preventDefault();
 
         try {
-            await axios.post("https://management-system-g7rv.onrender.com/api/v1/form/createForm", data, {withCredentials: true});
+            await axios.post("https://management-system-g7rv.onrender.com/api/v1/form/createForm", data);
 
             // Creating PDF Api call
-            await axios.post('https://management-system-g7rv.onrender.com/api/v1/form/createPdf', data, {withCredentials: true});
+            await axios.post('https://management-system-g7rv.onrender.com/api/v1/form/createPdf', data);
             // Sending PDF to mail
-            const sendPdfResponse = await axios.post("https://management-system-g7rv.onrender.com/api/v1/form/sendPdfToMail", null, {withCredentials: true});
+            const sendPdfResponse = await axios.post("https://management-system-g7rv.onrender.com/api/v1/form/sendPdfToMail");
 
             alert(sendPdfResponse.data);
         } catch (error) {
--- server/src/app.js
+++ server/src/app.js
@@ -4,14 +4,11 @@ import cors from 'cors';
 
 const app= express();
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}));
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(".src/public"));
 
 
 // All Routes //
